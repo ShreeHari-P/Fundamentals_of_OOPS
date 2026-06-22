71d8d014e9b6a46e4aeb0d885d9e7d6a9b4c3108
Shree Hari P <shreehariparthiban159@gmail.com>
2026-06-22 11:36:20 +0530

    Multiple Interface Implementation

---
+++ Employment Management/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SoftwareEngineer software = new SoftwareEngineer("Shree");
+        software.work();
+        software.attendTraining();
+        software.submitReport();
+    }
+}   
