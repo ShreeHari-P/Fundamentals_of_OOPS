ecfa8b4e493779c5c46c08ebbf0af9c1becf880a
Shree Hari P <shreehariparthiban159@gmail.com>
2026-07-08 21:59:06 +0530

    compareTo() and Comparable Method implementation

--- Book.java
+++ Book.java
@@ -1,25 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Book {
-    String title;
-    String author;
+class Read implements Comparable<Read>{
+    private String title;
+    private String author;
+    private int pages;
 
-    public Book(String title, String author){
+    public Read(String title, String author, int pages){
         this.title = title;
         this.author = author;
+        this.pages = pages;
     }
 
-    public void bookInfo(){
-        System.out.println("The Book name is " + title + " and Written by " + author);
+    public String getTitle(){return title;}
+    public String getAuthor(){return author;}
+    public int getPages(){return pages;}
+
+    @Override
+    public int compareTo(Read other){
+        if(this.pages != other.pages){
+            return Integer.compare(other.pages, this.pages);
+        }
+        return this.title.compareTo(other.title);
     }
+
+    @Override
+    public String toString(){
+        return "Book[title="+title+", author="+author+", pages="+pages+"]";
+    }
+}
+
+public class Book {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        
-        String title = scan.nextLine();
-        String author = scan.nextLine();
 
-        Book b = new Book(title, author);
-        b.bookInfo();
-        scan.close();        
-    }
+        ArrayList<Read> read = new ArrayList<>();
+        
+        String[] parts1 = scan.nextLine().split(",");
+        String[] parts2 = scan.nextLine().split(",");
+        String[] parts3 = scan.nextLine().split(",");
+
+        read.add(new Read(parts1[0], parts1[1], Integer.parseInt(parts1[2])));
+        read.add(new Read(parts2[0], parts2[1], Integer.parseInt(parts2[2])));
+        read.add(new Read(parts3[0], parts3[1], Integer.parseInt(parts3[2])));
+
+        Collections.sort(read);
+
+        for(Read myRead : read){
+            System.out.println(myRead);
+        }
+
+    }    
 }
